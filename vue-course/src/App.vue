db41124a5e36a4ae381b295d52ddfd6be9652e4b
Paata Home <paata.tepnadze@gmail.com>
2023-03-23 18:41:10 +0100

    create component post and post form

--- vue-course/src/App.vue
+++ vue-course/src/App.vue
@@ -1,34 +1,18 @@
 <template>
   <div class="app">
-    <div>
-      <form @submit.prevent>
-        <h4>create post</h4>
-        <input
-          v-bind:value="title"
-          @input="title = $event.target.value"
-          class="input"
-          type="text"
-          placeholder="title"
-        />
-        <input
-          v-bind:value="body"
-          @input="body = $event.target.value"
-          class="input"
-          type="text"
-          placeholder="post"
-        />
-        <button class="btn" @click="createPost">create</button>
-      </form>
-    </div>
-    <div class="post" v-for="post in posts">
-      <div><strong>Title: </strong> {{ post.title }}</div>
-      <div><strong>DEscription: </strong> {{ post.body }}</div>
-    </div>
+    <post-form />
+    <post-list :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
 export default {
+  components: {
+    PostForm,
+    PostList,
+  },
   data() {
     return {
       posts: [
@@ -75,28 +59,5 @@ export default {
 }
 .app {
   padding: 20px;
-}
-form {
-  display: flex;
-  flex-direction: column;
-}
-.btn {
-  align-self: flex-end;
-  margin-top: 15px;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 2px solid teal;
-}
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-.input {
-  width: 100%;
-  border: 2px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
 }
 </style>
---
+++ vue-course/src/components/PostForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <h4>create post</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="title"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="post"
+      />
+      <button class="btn">create</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 2px solid teal;
+}
+
+.input {
+  width: 100%;
+  border: 2px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
