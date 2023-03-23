d2ba63f2ade75f21eec752e728f8453d1c2c8ec4
Paata Home <paata.tepnadze@gmail.com>
2023-03-23 15:10:55 +0100

    intialize project . create like dislike onclick increaser

---
+++ vue-course/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      Amount of Likes <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Amount of Dislikes <strong>{{ dislikes }}</strong>
+    </div>
+    <div>
+      <button @:click="addLike">Like</button>
+      <button @click="addDislikes">Dislike</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislikes() {
+      this.dislikes += 1;
+    },
+  },
+};
+</script>
+
+<style></style>
